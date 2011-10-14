0b5c6b15f71b485c9ea0fbbc994618ead2d18916
fifyyulianty <fifi_maniz08@yahoo.com>
2011-10-14 12:42:39 +0700

    editsesi5

--- Praktikum/sesi5/src/perpustakaan/Perpustakaan.java
+++ Praktikum/sesi5/src/perpustakaan/Perpustakaan.java
@@ -2,6 +2,7 @@ public class Perpustakaan {
 	private String id;
 	private String peminjam;
 	private String judul;
+	private static Integer Jumlah;
 	
 	public String getJudul() {
 	return judul;
@@ -21,4 +22,10 @@ public String getId(){
 public void setId(String id) {
 	this.id=id;
 	}
+public static Integer getJumlah(){
+	return Jumlah;
+}
+public static void setJumlah(Integer Jumlah){
+	Perpustakaan.Jumlah=Jumlah;
+}
 }
--- Praktikum/sesi5/src/perpustakaan/PerpustakaanMain.java
+++ Praktikum/sesi5/src/perpustakaan/PerpustakaanMain.java
@@ -18,13 +18,15 @@ public class PerpustakaanMain{
 		fis = new FileInputStream(file);
 		bis = new BufferedInputStream(fis);
 		dis = new DataInputStream(bis);
-		
+		int count=0;
 		while(dis.available() !=0){
+			count ++;
 			//memecah text perbaris menjadi beberapa kolom sesuai dengan pemisah koma
 			String line = dis.readLine();
 			String[] data = line.split(", ");
 			
 			Perpustakaan p= new Perpustakaan();
+			p.setJumlah(count);
 			p.setId(data[0]);
 			p.setPeminjam(data[1]);
 			p.setJudul(data[2]);
@@ -36,6 +38,7 @@ public class PerpustakaanMain{
 				System.out.println("Exception : " + ex.getMessage());
 			}
 			for (Perpustakaan perpus : listPerpustakaan) {
+				System.out.println(perpus.getJumlah());
 				System.out.println("========================");
 				System.out.println("ID : " + perpus.getId());
 				System.out.println("Penyewa : " + perpus.getPeminjam());
